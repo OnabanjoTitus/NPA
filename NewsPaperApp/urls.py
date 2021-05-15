dc188bc6cd1a7cf372ca63f290f21b7a2d818903
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-05-15 13:47:53 +0100

    updates

--- NewsPaperApp/urls.py
+++ NewsPaperApp/urls.py
@@ -20,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls')),
-    path('articles/', include('articles.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('users/', include('users.urls')),
+                  path('users/', include('django.contrib.auth.urls')),
+                  path('', include('pages.urls')),
+                  path('articles/', include('articles.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- articles/models.py
+++ articles/models.py
@@ -8,6 +8,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    cover = models.ImageField(upload_to='images/', blank=True)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
